aad22bcb97e270b46506693f9db92428312ef296
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-05-07 15:27:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsletter</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="text-center">
+                <h1>Newsletter</h1>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <form action="" method="post">
+            <div>
+                <p class="email-input m-5">
+                <style>
+                    .email-input{
+                        width: 300px;
+                    }
+                </style>
+                    <label for="email" class="form-label">Indirizzo email</label>
+                    <input type="text" id="email" name="email" class="form-control">
+                </p>
+            </div>
+        </form>
+    </main>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
